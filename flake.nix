1529da1975b7ca13d7938157174af8c3db8792e9
eveeifyeve <eveeg1971@gmail.com>
2024-08-09 21:16:29 +1000

    feat: nix setup

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devenv.url = "github:cachix/devenv";
+  };
+  outputs =
+    inputs@{ flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ inputs.devenv.flakeModule ];
+      systems = nixpkgs.lib.systems.flakeExposed;
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          lib,
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          devenv.shells.default = {
+            dotenv.enable = false;
+            packages =
+              with pkgs;
+              [ ]
+              ++ (with pkgs.nodePackages; [
+                vscode-langservers-extracted
+                typescript-language-server
+              ]);
+            languages = {
+              typescript.enable = true;
+              javascript = {
+                enable = true;
+                bun = {
+                  enable = true;
+                  install.enable = true;
+                };
+              };
+            };          
+          };
+        };
+    };
+}
